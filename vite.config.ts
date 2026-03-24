9c05af0c7feacdd3474ef56515201e1bab28659e
Ahmed Abdelsattar Radwan <ta3alam.tech2024@gmail.com>
2026-03-24 23:00:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,15 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 3000,
   },
 });
