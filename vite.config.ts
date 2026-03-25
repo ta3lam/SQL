0f824c2c6d224ff266b33971da8bdc7b518e4cc3
Ahmed Abdelsattar Radwan <ta3alam.tech2024@gmail.com>
2026-03-25 02:02:14 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,12 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-    assetsInlineLimit: 100000000,
   },
 });
